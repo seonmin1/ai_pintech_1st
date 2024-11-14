c0146351df6aed721efa00050ec03cb9fa0dfb01
seonmin1 <iseonmin161@gmail.com>
2024-11-14 18:01:49 +0900

    수업소스 추가

---
+++ exam05/src/main/java/org/koreait/Exam05Application.java
@@ -0,0 +1,14 @@
+package org.koreait;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exam05Application {
+
+	public static void main(String[] args) {
+		System.out.println("메인 메서드!!!!");
+		SpringApplication.run(Exam05Application.class, args);
+	}
+
+}
