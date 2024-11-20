5ebb855f170fad914a92af9cb97d5d81612f0416
seonmin1 <iseonmin161@gmail.com>
2024-11-20 17:26:30 +0900

    Delete 7. 스프링 부트/exam05 directory

--- 7. 스프링 부트/exam05/src/main/java/org/koreait/global/configs/MvcConfig.java
+++
@@ -1,9 +0,0 @@
-package org.koreait.global.configs;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class MvcConfig implements WebMvcConfigurer { // WebMvcConfigurer 인터페이스 구현 필수!
-
-}
