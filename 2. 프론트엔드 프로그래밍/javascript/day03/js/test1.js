3cb5577f380f4cd172aca4a0967cabf95c0fed69
seonmin1 <iseonmin161@gmail.com>
2024-12-23 09:31:18 +0900

    day03 수업소스

---
+++ 2. 프론트엔드 프로그래밍/javascript/day03/js/test1.js
@@ -0,0 +1,8 @@
+/* window.onload = function () {
+  alert("test1");
+}; */
+
+// 이벤트 리스너
+window.addEventListener("load", function () {
+  alert("test1");
+});
---
+++ 2. 프론트엔드 프로그래밍/javascript/day03/js/test2.js
@@ -0,0 +1,8 @@
+/* window.onload = function () {
+  alert("test2");
+}; */
+
+// 이벤트 리스너
+window.addEventListener("load", function () {
+  alert("test2");
+});
